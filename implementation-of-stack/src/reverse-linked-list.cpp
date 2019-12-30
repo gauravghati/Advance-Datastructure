4d89d7b3f0c9caad3f19952ea6b696d25d6551d8
gauravghati <gauravghati225@gmail.com>
2019-12-30 12:05:09 +0530

    whole

--- implementation-of-stack/src/reverse-linked-list.cpp
+++ implementation-of-stack/src/reverse-linked-list.cpp
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #include <string>
+#include <math.h>
 #include "Stack.h"
 #include "Stack.cpp"
 using namespace std;
@@ -15,17 +16,151 @@ using namespace std;
 
 int priority(char);
 char* postfixlogic(char[MAX]);
+int operators(int, int, char);
+int eval(char*);
+char * reverse(char*);
+bool checkinfix(char*);
 
 class Expression{
 	char arr[MAX];
 	public:
 		void toprefix();
 		void topostfix();
+		void evalpost();
+		void evalpre();
 		void enterdata();
 		void display();
 };
 
+void Expression :: enterdata(){
+	cout << "Enter Expression: ";
+	cin.getline(arr, MAX);
+}
+
+void Expression :: evalpost(){
+	cout << "\nEvaluation of Postfix is:" << eval(arr) << "\n";
+}
+
+void Expression :: evalpre(){
+
+	char * chgArr = reverse(arr);
+
+	cout << "\nEvaluation of Prefix is:" << eval(chgArr) << "\n";
+}
+
 void Expression :: toprefix(){
+
+	char * chgArr = reverse(arr);
+	chgArr = postfixlogic(chgArr);
+	chgArr = reverse(chgArr);
+
+	if(checkinfix(arr)){
+		cout << "\nPREFIX EXPRESSION: " << chgArr;
+	} else {
+		cout << "ENTER VALID INFIX EXPRESSION:";
+	}
+}
+
+void Expression :: topostfix(){
+	char *postfix = postfixlogic(arr);
+
+	if(checkinfix(arr)){
+		cout << "POSTFIX EXPRESSION: "<< postfix;
+	} else {
+		cout << "ENTER VALID INFIX EXPRESSION:";
+	}
+}
+
+void Expression :: display(){
+	int i=0;
+	while(arr[i] != '\0'){
+		cout << arr[i];
+		i++;
+	}
+}
+
+
+int eval(char* arr){
+	Stack<int> s;
+	int i=0, val;
+
+	while(arr[i]!='\0'){
+		if(arr[i]>=48 && arr[i]<=57){
+			s.push(arr[i]-48);
+		} else if((arr[i] >= 65 && arr[i] <= 91) || (arr[i] >= 97 && arr[i] <= 129)){
+			cout << "Enter value numeric of "<<arr[i] << ": ";
+			cin >> val;
+			s.push(val);
+		} else if(!s.empty()) {
+			s.push(operators(s.pop(), s.pop(), arr[i]));
+		}
+		i++;
+	}
+	return s.pop();
+}
+
+bool checkinfix(char * arr){
+	int i=0;
+	int var = 0, var2 = 0;
+	Stack<char> s;
+	while(arr[i]!='\0'){
+		if((arr[i] >= 65 && arr[i] <= 91) || (arr[i] >= 97 && arr[i] <= 129)
+				|| (arr[i]>=48 && arr[i]<=57 )){
+			var++;
+		} else {
+			var2++;
+		}
+		i++;
+	}
+
+	if(var-1!=var2)
+		return false;
+
+	i=0;							// for "(" and ")"
+	while(arr[i]!='\0'){
+		if(arr[i]=='('){
+			s.push(arr[i]);
+		} else if(arr[i]==')') {
+			if(s.empty())
+				return false;
+			s.pop();
+		}
+		i++;
+	}
+	if(!s.empty())
+		return false;
+
+	i=0;
+	while(arr[i]!='\0'){
+		i++;
+	}
+	i = i - 1;
+	if(!(((arr[0] >= 65 && arr[0] <= 91) || (arr[0] >= 97 && arr[0] <= 129)
+				|| (arr[0]>=48 && arr[0]<=57 )) && ((arr[i] >= 65 && arr[i] <= 91)
+				|| (arr[i] >= 97 && arr[i] <= 129) || (arr[i]>=48 && arr[i]<=57 )))){
+		return false;
+	}
+
+	return true;
+}
+
+int operators(int ope1, int ope2, char op){
+	switch(op){
+		case '+':
+			return (ope1+ope2);
+		case '-':
+			return (ope1-ope2);
+		case '*':
+			return (ope1*ope2);
+		case '/':
+			return (ope1/ope2);
+		case '^':
+			return pow(ope1,ope2);
+	}
+	return 0;
+}
+
+char * reverse(char* arr){
 	Stack<char> s;
 	int i=0;
 	char* chgArr = new char[MAX];
@@ -49,37 +184,7 @@ void Expression :: toprefix(){
 			chgArr[i] = '(';
 		i++;
 	}
-
-	chgArr = postfixlogic(chgArr);
-
-	i=0;
-	while(chgArr[i]!='\0'){
-		s.push(chgArr[i]);
-		i++;
-	}
-
-	i=0;
-	while(!s.empty()){
-		chgArr[i] = s.pop();
-		i++;
-	}
-
-    cout << "\nPREFIX EXPRESSION: " << chgArr;
-}
-
-void Expression :: display(){
-	int i=0;
-	while(arr[i] != '\0'){
-		cout << arr[i];
-		i++;
-	}
-}
-
-void Expression :: topostfix(){
-	char *postfix = postfixlogic(arr);
-
-    int	i=0;
-    cout << "POSTFIX EXPRESSION: "<< postfix;
+	return chgArr;
 }
 
 char* postfixlogic(char* arr){
@@ -97,7 +202,8 @@ char* postfixlogic(char* arr){
 				j++;
 			}
 			s.pop();
-		} else if((arr[i] >= 65 && arr[i] <= 91) || (arr[i] >= 97 && arr[i] <= 129)){
+		} else if((arr[i] >= 65 && arr[i] <= 91) || (arr[i] >= 97 && arr[i] <= 129)
+				|| (arr[i]>=48 && arr[i]<=57 )){
 			postfix[j] = arr[i];
 			j++;
 		}
@@ -141,26 +247,48 @@ int priority(char a){
 	return 0;
 }
 
-void Expression :: enterdata(){
-	cout << "Enter Infix String: ";
-	cin.getline(arr, MAX);
-}
-
 void menu(){
-	cout << "1) Enter Infix Expression";
-	cout << "2) Convert Expression to postfix";
-	cout << "3) Convert Expression to prefix";
-	cout << "4) Evaluation of postfix";
-	cout << "5) Evaluation of prefix";
-	cout << "6) Exit";
+	cout << "\n1) Convert Expression to postfix";
+	cout << "\n2) Convert Expression to prefix";
+	cout << "\n3) Evaluation of postfix";
+	cout << "\n4) Evaluation of prefix";
+	cout << "\n5) Exit";
 }
 
 int main() {
 
 	Expression ex;
-	ex.enterdata();
-	ex.topostfix();
-	ex.toprefix();
 
+	int ch;
+
+	while(ch!=5){
+
+		menu();
+		cout << "\nEnter Choice: ";
+		cin >> ch;
+		cin.ignore(1);
+
+		if(ch!=5)
+			ex.enterdata();
+
+		switch(ch){
+			case 1:
+				ex.topostfix();
+				break;
+			case 2:
+				ex.toprefix();
+				break;
+			case 3:
+				ex.evalpost();
+				break;
+			case 4:
+				ex.evalpre();
+				break;
+			case 5:
+				break;
+			default:
+				cout << "INVALID CHOICE!";
+		}
+	}
 	return 0;
 }
