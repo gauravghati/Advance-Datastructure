5d25bd318519cb5304fd7429e68d611aaf26b722
gauravghati <gauravghati225@gmail.com>
2020-01-09 17:59:39 +0530

    queue complete

--- queue/src/Queue.cpp
+++ queue/src/Queue.cpp
@@ -20,20 +20,23 @@ void Queue<T> :: enque(T var){
 	Node<T> * p;
 	p = new Node<T>;
 
-	if(front==NULL){
-		front = p;
-	}
-
 	p -> data = var;
 	p -> next = NULL;
-	rear -> next = p;
+	
+	if(front==NULL){
+		front = p;
+		rear = p;
+	} else {
+		rear -> next = p;
+		rear = rear -> next;
+	}
 }
 
 template <class T>
 void Queue<T> :: printQ(){
 	Node<T> *temp = front;
-	while(temp != rear){
-		cout << temp->data;
+	while(temp != NULL){
+		cout << (temp->data);
 		temp = temp->next;
 	}
 }
@@ -43,6 +46,15 @@ T Queue<T> :: deque(int index){
 	Node<T> *temp, *prev;
 	T data;
 	temp = front;
+
+	data = temp->data;
+	index = index - 1;
+
+	if(index==0){
+		front = temp->next;
+		delete(temp);
+		return data;
+	}
 
 	while(index!=0) {
 		prev = temp;
@@ -58,17 +70,13 @@ T Queue<T> :: deque(int index){
 }
 
 template <class T>
-Queue<T>::~Queue() {
-
-}
-
-template <class T>
 int Queue<T>:: size() {
 	int s = 0;
 	Node<T> *temp = front;
 
-	while(temp!=rear){
+	while(temp!=NULL){
 		s++;
+		temp = temp -> next;
 	}
 	return s;
 }
@@ -76,9 +84,14 @@ int Queue<T>:: size() {
 template <class T>
 T Queue<T> :: getele(int index){
 	Node<T> *temp = front;
-
+	index--;
 	while(index--){
 		temp = temp->next;
 	}
 	return temp->data;
+}
+
+template <class T>
+Queue<T>::~Queue() {
+	
 }
--- queue/src/queue.cpp
+++ queue/src/queue.cpp
@@ -1,25 +1,35 @@
-//============================================================================
-// Name        : queue.cpp
-// Author      : Gaurav
-// Version     :
-// Copyright   : 
-// Description : Hello World in C++, Ansi-style
-//============================================================================
+/*
+=========================================================================
+Assignment : Implementation of Priority Queue
+Name : Gaurav Ghati
+class : SE 10
+Batch : F 10
+
+Problem Statement:
+Implement priority queue as ADT using single linked list for servicing
+patients in an hospital with priorities as 
+	i) Serious (top priority) 
+	ii) medium illness (medium priority) 
+	iii) General (Least priority)
+
+=========================================================================
+*/
 
 #include <iostream>
 #include "Queue.cpp"
+#include "Queue.h"
 #include <string.h>
 using namespace std;
 
-int getprio(char*);
+int getprio(string);
 
 class Patient{
-	char* mobileno;			// mobile no
-	int pid;				// patient id
-	char* bgrp;				// blood group
-	int prio;				// priority
-	char* name;				// name and disease
-	char* disease;
+	string mobileno;			// mobile no
+	int pid;					// patient id
+	string bgrp;				// blood group
+	int prio;					// priority
+	string name;				// name and disease
+	string disease;
 
 	public:
 		Patient();
@@ -41,14 +51,15 @@ int Patient :: getpriority(){
 	return prio;
 }
 
-
 void Patient :: scan(){
 	cin.ignore();
 	cout << "Enter Name of Person: ";
-	cin.getline(name, 20);
+	//cin.getline(name, 20);
+	cin >> name;
 
 	cout << "Enter Disease: ";
-	cin.getline(disease, 20);
+	//cin.getline(disease, 20);
+	cin >> disease;
 
 	prio = getprio(disease);
 
@@ -57,21 +68,23 @@ void Patient :: scan(){
 	
 	cout << "Enter Mobile Number: ";
 	cin.ignore();
-	cin.getline(mobileno, 10);
+	//cin.getline(mobileno, 10);
+	cin >> mobileno;
 
 	cout << "Enter Blood Group: ";
-	cin.getline(bgrp, 3);
+	//cin.getline(bgrp, 3);
+	cin >> bgrp;
 }
 
 ostream & operator<<(ostream & Str, Patient const & v) {
 	Str << "\n<---- Patient Detail ----->";
 	Str << "\nName : " << v.name;
 	Str << "\nDisease : " << v.disease;
-	Str << "\nPriority : " << v.prio;
+	Str << "\nPriority : " << ((v.prio==3)? "(HIGH)":((v.prio==2)? "(MEDIUM)":"(LOW)"));
 	Str << "\nPatient ID : " << v.pid;
 	Str << "\nMobile Number : " << v.mobileno;
 	Str << "\nBlood Group : " << v.bgrp;
-	Str << "\n-----------------------------";
+	Str << "\n---------------------------";
 	return Str;
 }
 
@@ -82,12 +95,12 @@ void menu(){
 	cout << "\n4) Exit";
 }
 
-int getprio(char* dname){
-	if(!(strcmp(dname,"accident") && strcmp(dname,"heart_attack")
-			&& strcmp(dname,"panic_attack") && strcmp(dname,"fracture"))){
+int getprio(string dname){
+	if(!(dname.compare("accident") && dname.compare("heart_attack")
+			&& dname.compare("panic_attack") && dname.compare("fracture"))){
 		return 3;
 	}
-	else if(!(strcmp(dname,"flu") && strcmp(dname,"cough") && strcmp(dname,"viral"))){
+	else if(!(dname.compare("flu") && dname.compare("cough") && dname.compare("viral"))){
 		return 2;
 	} else {
 		return 1;
@@ -95,10 +108,12 @@ int getprio(char* dname){
 }
 
 int main() {
-
 	Patient p;
-/*	Queue<Patient> patQ;
+	Queue<Patient> patQ;
 	int val = 0;
+	int maxp = 0;
+	int i_at_max = 0;
+	int s = 0;
 
 	while(val != 4){
 
@@ -113,38 +128,203 @@ int main() {
 				cout << "\nPateint Added!";
 				break;
 			case 2:
-				int maxp = 0;
-				int i_at_max = 0;
-				int s = patQ.size();
-
+				maxp = 0;
+				i_at_max = 0;
+				s = patQ.size();
+				cout << "\nNEXT PATIENT";
 				for(int i=0; i<s; i++){
-					p = patQ.getele(i);
+					p = patQ.getele(i+1);
 					if(maxp < p.getpriority()){
 						maxp = p.getpriority();
-						i_at_max = i;
+						cout << maxp;
+						i_at_max = i+1;
 					}
 				}
-				patQ.deque(i_at_max);
+				cout << patQ.deque(i_at_max);
 				break;
-			//case 3:
-			//break;
+			case 3:
+				cout << "=======================================";
+				cout << "\nLIST OF ALL PATIENTS IN QUEUE: ";
+				patQ.printQ();
+				cout << endl;
+				cout << "=======================================";
+				break;
 		}
-	} */
-
-	Queue<int> test;
-
-	int qn = 0, t;
-	cout << "no. of ele:";
-	cin >> qn;
-
-	while(qn--){
-		cout << "enter ele: ";
-		cin >> t;
-		test.enque(t);
-	}
-
-	test.printQ();
+	} 
 
 	return 0;
 }
 
+/*
+
+OUTPUT:
+
+1) Enter Patient Into Queue
+2) Serve next Patient to the Doctor
+3) Display List of all patient
+4) Exit
+Enter Choice: 1
+
+Enter Name of Person: gaurav
+Enter Disease: flu
+Enter Patient ID: 101
+Enter Mobile Number: 902341487 
+Enter Blood Group: o+
+
+Pateint Added!
+
+1) Enter Patient Into Queue
+2) Serve next Patient to the Doctor
+3) Display List of all patient
+4) Exit
+Enter Choice: 1
+
+Enter Name of Person: sahil
+Enter Disease: accident 
+Enter Patient ID: 102 
+Enter Mobile Number: 9128379142
+Enter Blood Group: ab+
+
+Pateint Added!
+
+1) Enter Patient Into Queue
+2) Serve next Patient to the Doctor
+3) Display List of all patient
+4) Exit
+Enter Choice: 1
+
+Enter Name of Person: raje
+Enter Disease: head_pain
+Enter Patient ID: 104  
+Enter Mobile Number: 293746823
+Enter Blood Group: a-
+
+Pateint Added!
+
+1) Enter Patient Into Queue
+2) Serve next Patient to the Doctor
+3) Display List of all patient
+4) Exit
+Enter Choice: 1
+
+Enter Name of Person: shreyas
+Enter Disease: heart_attack
+Enter Patient ID: 2001
+Enter Mobile Number: 29384928 
+Enter Blood Group: b- 
+
+Pateint Added!
+
+1) Enter Patient Into Queue
+2) Serve next Patient to the Doctor
+3) Display List of all patient
+4) Exit
+Enter Choice: 3
+
+=======================================
+LIST OF ALL PATIENTS IN QUEUE: 
+<---- Patient Detail ----->
+Name : gaurav
+Disease : flu
+Priority : (MEDIUM)
+Patient ID : 101
+Mobile Number : 902341487
+Blood Group : o+
+---------------------------
+<---- Patient Detail ----->
+Name : sahil
+Disease : accident
+Priority : (HIGH)
+Patient ID : 102
+Mobile Number : 9128379142
+Blood Group : ab+
+---------------------------
+<---- Patient Detail ----->
+Name : raje
+Disease : head_pain
+Priority : (LOW)
+Patient ID : 104
+Mobile Number : 293746823
+Blood Group : a-
+---------------------------
+<---- Patient Detail ----->
+Name : shreyas
+Disease : heart_attack
+Priority : (HIGH)
+Patient ID : 2001
+Mobile Number : 29384928
+Blood Group : b-
+---------------------------
+=======================================
+
+1) Enter Patient Into Queue
+2) Serve next Patient to the Doctor
+3) Display List of all patient
+4) Exit
+Enter Choice: 2
+
+NEXT PATIENT
+<---- Patient Detail ----->
+Name : sahil
+Disease : accident
+Priority : (HIGH)
+Patient ID : 102
+Mobile Number : 9128379142
+Blood Group : ab+
+---------------------------
+
+1) Enter Patient Into Queue
+2) Serve next Patient to the Doctor
+3) Display List of all patient
+4) Exit
+Enter Choice: 2
+
+NEXT PATIENT
+<---- Patient Detail ----->
+Name : shreyas
+Disease : heart_attack
+Priority : (HIGH)
+Patient ID : 2001
+Mobile Number : 29384928
+Blood Group : b-
+---------------------------
+
+1) Enter Patient Into Queue
+2) Serve next Patient to the Doctor
+3) Display List of all patient
+4) Exit
+Enter Choice: 2
+
+NEXT PATIENT
+<---- Patient Detail ----->
+Name : gaurav
+Disease : flu
+Priority : (MEDIUM)
+Patient ID : 101
+Mobile Number : 902341487
+Blood Group : o+
+---------------------------
+
+1) Enter Patient Into Queue
+2) Serve next Patient to the Doctor
+3) Display List of all patient
+4) Exit
+Enter Choice: 2
+
+NEXT PATIENT
+<---- Patient Detail ----->
+Name : raje
+Disease : head_pain
+Priority : (LOW)
+Patient ID : 104
+Mobile Number : 293746823
+Blood Group : a-
+---------------------------
+
+1) Enter Patient Into Queue
+2) Serve next Patient to the Doctor
+3) Display List of all patient
+4) Exit
+Enter Choice: 4
+
+*/
